62852d94a19db98ed3c92f5acf29468911a363eb
Calvin-T <calvin+1@sliame.com>
2020-06-04 16:42:57 +0200

    test

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,30 +1,87 @@
 import uuid from 'uuid'
+import database from '../firebase/firebase'
 
-export const addExpense = ( // This is being added each time, i confuse expenses with expense, expenses no longer exists.
-    {
-        description = '', // Destructuring user inputs and setting defaults
-        note = '',
-        amount = 0,
-        createdAt = 0
-    } = {}
-) => ({ // Then attach them to the object
+export const addExpense = (expense) => ({ // Then attach them to the object
     type: 'ADD_EXPENSE',
-    expense: {
-        id: uuid(),
-        description,
-        note,
-        amount,
-        createdAt
-    }
+    expense
 })
+
+export const startAddExpense = (expenseData = {}) => {
+    return (dispatch, getState) => { // dispatch object only works because of THUNK
+        const uid = getState().auth.uid
+        const {
+                description = '', // Destructuring user inputs and setting defaults
+                note = '',
+                amount = 0,
+                createdAt = 0
+        } = expenseData
+        
+        const expense = { description, note, amount, createdAt } // setting defaults for object
+        
+        return database.ref(`users/${uid}/expenses`).push(expense).then((ref) => { // pushing to database
+            dispatch(addExpense({
+                id: ref.key,
+                ...expense
+            }))
+        })
+    }
+}
 
 export const removeExpense = ({ id } = {}) => ({
     type: 'REMOVE_EXPENSE',
     id
 })
 
+export const startRemoveExpense = ({ id } = {}) => {
+    return (dispatch, getState) => { // dispatch gets passed from redux lib
+        const uid = getState().auth.uid
+        return database.ref(`users/${uid}/expenses/${id}`).remove().then(() => { // return promise, then remove from database
+            dispatch(removeExpense({ id })) // once done, remove from redux STORE
+        })
+    }
+}
+
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
     id,
     updates
 })
+
+export const startEditExpense = (id, updates) => {
+    return (dispatch, getState) => {
+        const uid = getState().auth.uid
+        return database.ref(`users/${uid}/expenses/${id}`).update(updates).then(() => {
+            dispatch(editExpense(id, updates))
+        })
+    }
+}
+
+// set expenses
+
+export const setExpenses = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+})
+
+// SOMETHING THAT NEEDS TO BE LEARNED
+// connecting REDUX To a database, api calls etc.
+
+export const startSetExpenses = () => { // fetching and parsing data.
+    return (dispatch, getState) => {
+        const uid = getState().auth.uid
+
+        return database.ref(`users/${uid}/expenses`) // calling the database coloum
+            .once('value') // one time
+            .then((snapshot) => { // then taking the snapshot received
+                const expenses = [] // creating an array to fill later
+
+                snapshot.forEach((childSnapshot) => { // looping over returned data
+                    expenses.push({ // filling the premade array with
+                        id: childSnapshot.key, // the keys returned by Firebase
+                        ...childSnapshot.val() // spreading all the data inside those database keys
+                    })
+                })
+                dispatch(setExpenses(expenses))
+            })
+    }
+}
--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import AppRouter from './routers/AppRouter'
+import AppRouter, { history } from './routers/AppRouter'
 import configureStore from './store/configureStore'
-import {addExpense} from './actions/expenses'
-import {setTextFilter} from './actions/filters'
+import {startSetExpenses} from './actions/expenses'
+import { login, logout } from './actions/auth'
 import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css';
+import { firebase } from './firebase/firebase'
 
 const store = configureStore()
 
@@ -18,4 +19,30 @@ const jsx = ( // This gives the whole app access to the store as a prop
     </Provider>
 )
 
-ReactDOM.render(jsx, document.getElementById('app'))
+let hasRendered = false
+const renderApp = () => {
+    if(!hasRendered) {
+        ReactDOM.render(jsx, document.getElementById('app')) // only runs that app if they're logged in
+        hasRendered = true
+    } else {
+
+    }
+}
+
+ReactDOM.render(<p>Loading Data...</p>, document.getElementById('app'))
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        store.dispatch(login(user.uid))
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp()
+            if (history.location.pathname === '/') {
+                history.push('/dashboard')
+            }
+        })
+    } else {
+        store.dispatch(logout())
+        renderApp()
+        history.push('/')
+    }
+})
